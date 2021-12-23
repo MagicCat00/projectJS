76f515d3a10aa3472bccaae124fbb80a17064fe7
Yuliya <vsggdfaf@gmail.com>
2021-12-23 12:06:22 +0300

    присвоила значение переменным и использовала методы и свойства

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 const a = 1
 const b = "Вторая переменная"
 
-const logget = () => {
-    console.log('Мы узнали все команды');
-}
+const title = "ProjectJS"
+console.log(title);
+console.log(typeof title);
+
+const screens = "Простые, Сложные, Интерактивные"
+console.log(screens);
+console.log(typeof screens);
+console.log(screens.split(", "));
+
+const screenPrice = 15000
+console.log("Стоимость верстки экранов " + screenPrice + "рублей/долларов/гривен/юани");
+
+const rollback = 50
+console.log(rollback);
+
+const fullPrice = 50000
+console.log("Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани");
+console.log(fullPrice * (rollback/100));
+
+
+const adaptive = "true"
+console.log(adaptive);
+console.log(typeof adaptive);
